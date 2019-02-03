634077a0b50aebc12a4ec695058faebabe1b913e
jams <sindhujambunathan@gmail.com>
2019-02-02 23:43:28 -0800

    Completed the module

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -17,8 +17,18 @@ namespace GradeBook.GradeBooks
 
             foreach( Student student in Students)
             {
-                var threshold = Students.Count * 0.2;
+                var threshold = (int)Math.Ceiling( Students.Count * 0.2 );
+                var grades = Students.OrderbyDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToLis();
+                if (averageGrade > grades[threshold - 1])
+                    return 'A';
+                else if (averageGrade > grades[2 * threshold - 1])
+                    return 'B';
+                else if (averageGrade > grades[3 * threshold - 1])
+                    return 'C';
+                else if (averageGrade > grades[4 * threshold - 1])
+                    return 'D';
 
+                else return 'F';
 
             }
             return 'F';
