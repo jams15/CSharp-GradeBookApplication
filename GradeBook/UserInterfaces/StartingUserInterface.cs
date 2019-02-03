3f4070bbaff97df5058d976bf2df137041239ee6
jams <sindhujambunathan@gmail.com>
2019-02-03 00:37:48 -0800

    Completed the module

--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -40,7 +40,7 @@ namespace GradeBook.UserInterfaces
                 Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
                 return;
             }
-            var name = parts[3];
+            var name = parts[1];
             var type = parts[2].ToLower();
             BaseGradeBook gradeBook;
             if ( type == "standard" )
